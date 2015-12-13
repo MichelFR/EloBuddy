11779258e317d005b62189b1815ab6b7c8fe97e3
incaner <michelfr95@gmail.com>
2015-12-13 13:59:39 +0100

    Intelligent E use

--- CancerDarius/CancerDarius/Modes/Combo.cs
+++ CancerDarius/CancerDarius/Modes/Combo.cs
@@ -24,7 +24,10 @@ namespace CancerDarius.Modes
                 var target = TargetSelector.GetTarget(E.Range, DamageType.Physical);
                 if (E.IsReady() && target != null)
                 {
+                    if (Q.IsReady() || W.IsReady())
+                    {
                     E.Cast(target);
+                    }
                 }
             }
             if (Settings.UseW && W.IsReady())
