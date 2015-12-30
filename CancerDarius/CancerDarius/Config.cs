fe48702da85423d06c877fd9d84fdb49202cf824
incaner <michelfr95@gmail.com>
2015-12-30 19:47:10 +0100

    Update Config.cs

--- CancerDarius/CancerDarius/Config.cs
+++ CancerDarius/CancerDarius/Config.cs
@@ -218,7 +218,7 @@ namespace CancerDarius
                 }
                 public static bool ShowR
                 {
-                    get { return _ShowE.CurrentValue; }
+                    get { return _ShowR.CurrentValue; }
                 }
 
                 static Drawings()
